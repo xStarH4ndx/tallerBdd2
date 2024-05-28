46475d65177e2daaff757aa8f34aa1dccaaa0a75
xStarH4ndx <brunotoro1799@gmail.com>
2024-05-28 15:05:52 -0400

    terminado

--- Aburto_Toro.py
+++ Aburto_Toro.py
@@ -32,73 +32,73 @@ def crear_conexion():
 #Registro de usuario
 def registrar_usuario(conexion, nombre, email, password, tipo):
     try:
-        cur= conexion.cursor()
-        cur.execute("INSERT INTO usuario (nombre, email, password, tipo) VALUES (%s, %s, %s, %s)", (nombre, email, password, tipo))
+        consulta= conexion.cursor()
+        consulta.execute("INSERT INTO usuario (nombre, email, password, tipo) VALUES (%s, %s, %s, %s)", (nombre, email, password, tipo))
         conexion.commit()
-        print("\n---Usuario registrado con éxito---")
+        print("\n---USUARIO REGISTRADO---")
     except psycopg2.errors.UniqueViolation:
         conexion.rollback()
-        print("\n---El email ya existe---")
+        print("\n---EL EMAIL YA ESTÁ REGISTRADO---")
 
 #Login de usuario
 def login_usuario(conexion, email, password):
-    cur= conexion.cursor()
-    cur.execute("SELECT password, tipo FROM usuario WHERE email = %s", (email,))
-    resultado= cur.fetchone()
+    consulta= conexion.cursor()
+    consulta.execute("SELECT password, tipo FROM usuario WHERE email = %s", (email,))
+    resultado= consulta.fetchone()
     if ((resultado) and (resultado[0] == password)):#Si el resultado es != de None y es la misma contraseña, accede 
-        print("\n---Inicio de sesión exitoso---")
+        print("\n---SESION INICIADA---")
         return resultado[1]  #Devolver el tipo de usuario
     else:
-        print("---Email o contraseña incorrectos---")
+        print("---CREDENCIALES INCORRECTAS---")
         return None#En el caso contrario, no entra
 
 
 '''-------------------------OPCIONES USUARIO----------------------------'''
 
 def ver_informacion_personal(conexion, email):
-    cur= conexion.cursor()
-    cur.execute("SELECT nombre, email FROM usuario WHERE email = %s", (email,))
-    usuario = cur.fetchone()
+    consulta= conexion.cursor()
+    consulta.execute("SELECT nombre, email FROM usuario WHERE email = %s", (email,))
+    usuario = consulta.fetchone()
     if usuario:
-        print("\n---Información Personal---")
+        print("\n---INFORMACIÓN PERSONAL---")
         print(f"Nombre: {usuario[0]}")
         print(f"Email: {usuario[1]}")
     else:
-        print("\n---Usuario no encontrado---")
+        print("\n---USUARIO NO ENCONTRADO---")
 
 def listar_productos(conexion):
-    cur= conexion.cursor()
-    cur.execute("SELECT id_producto, nombre, descripcion, precio, cant_stock FROM producto ORDER BY id_producto")
-    productos= cur.fetchall()
-    print("\n---Catálogo de Productos---")
+    consulta= conexion.cursor()
+    consulta.execute("SELECT id_producto, nombre, descripcion, precio, cant_stock FROM producto ORDER BY id_producto")
+    productos= consulta.fetchall()
+    print("\n---CATÁLOGO DE PRODUCTOS---")
     for producto in productos:
         print(f"ID: {producto[0]}, Nombre: {producto[1]}, Descripción: {producto[2]}, Precio: {producto[3]}, Stock: {producto[4]}")
 
 def realizar_compra(conexion, email_user):
-    cur= conexion.cursor()
+    consulta= conexion.cursor()
     listar_productos(conexion)
-    producto_id= int(input("\nIngrese el ID del producto que desea comprar: "))
-    cantidad= int(input("Ingrese la cantidad: "))
+    producto_id= int(input("\ID de producto deseado: "))
+    cantidad= int(input("Cantidad a comprar: "))
 
     # Obtener el precio del producto y verificar stock
-    cur.execute("SELECT precio, cant_stock FROM producto WHERE id_producto = %s", (producto_id,))
-    producto= cur.fetchone()
+    consulta.execute("SELECT precio, cant_stock FROM producto WHERE id_producto = %s", (producto_id,))
+    producto= consulta.fetchone()
         
     if (producto) and (producto[1] >= cantidad): # Si el producto es != None y el stock del producto es mayor o igual a la cantidad de compra
         precio_total= producto[0] * cantidad
         nuevo_stock= producto[1] - cantidad
 
         #Insertar en la tabla venta
-        cur.execute("INSERT INTO venta (email, id_producto, monto_total) VALUES (%s, %s, %s)", (email_user, producto_id, precio_total))
+        consulta.execute("INSERT INTO venta (email, id_producto, monto_total) VALUES (%s, %s, %s)", (email_user, producto_id, precio_total))
         
         #Actualizar el stock del producto
-        cur.execute("UPDATE producto SET cant_stock = %s WHERE id_producto = %s", (nuevo_stock, producto_id))
+        consulta.execute("UPDATE producto SET cant_stock = %s WHERE id_producto = %s", (nuevo_stock, producto_id))
         
         conexion.commit()
-        print("\n---Compra realizada con éxito---")
+        print("\n---COMPRA REALIZADA---")
     else:
         conexion.rollback()
-        print("\n---Stock insuficiente o producto no encontrado---")
+        print("\n---NO HAY STOCK SUFICIENTE O NO EXISTE EL PRODUCTO---")
 
 
 '''-------------------------OPCIONES ADMINISTRADOR----------------------------'''
@@ -107,60 +107,60 @@ def realizar_compra(conexion, email_user):
 #producto, como nombre, descripción, precio y cantidad en stock.
 def registrar_producto(conexion):
     nombre= input("Nombre del producto: ")
-    descripcion= input("Descripción del producto: ")
-    precio= int(input("Precio del producto: "))
+    descripcion= input("Descripción: ")
+    precio= int(input("Precio: "))
     cant_stock= int(input("Cantidad en stock: "))
 
-    cur= conexion.cursor()
-    cur.execute("INSERT INTO producto (nombre, descripcion, precio, cant_stock) VALUES (%s, %s, %s, %s)", (nombre, descripcion, precio, cant_stock))
+    consulta= conexion.cursor()
+    consulta.execute("INSERT INTO producto (nombre, descripcion, precio, cant_stock) VALUES (%s, %s, %s, %s)", (nombre, descripcion, precio, cant_stock))
     conexion.commit()
-    print("\n---Producto registrado con éxito---")
+    print("\n---PRODUCTO REGISTRADO---")
 
 #El administrador puede buscar y ver los detalles de un 
 #producto en particular, incluyendo su información y cantidad en stock
 def ver_detalles_producto(conexion):
     mostrar_inventario(conexion)
-    producto_id= int(input("\nIngrese el ID del producto: "))
-    cur= conexion.cursor()
-    cur.execute("SELECT id_producto, nombre, descripcion, precio, cant_stock FROM producto WHERE id_producto = %s", (producto_id,))
-    producto= cur.fetchone()
+    producto_id= int(input("\nSeleccionar el ID del producto: "))
+    consulta= conexion.cursor()
+    consulta.execute("SELECT id_producto, nombre, descripcion, precio, cant_stock FROM producto WHERE id_producto = %s", (producto_id,))
+    producto= consulta.fetchone()
     if producto:
-        print("\n---Información del Producto---")
+        print("\n---INFORMACIÓN DEL PRODUCTO---")
         print(f"ID: {producto[0]}, Nombre: {producto[1]}, Descripción: {producto[2]}, Precio: {producto[3]}, Stock: {producto[4]}")
     else:
-        print("\n---Producto no encontrado---")
+        print("\n---PRODUCTO NO ENCONTRADO---")
 
 def mostrar_inventario(conexion):
-    cur= conexion.cursor()
-    cur.execute("SELECT id_producto, nombre FROM producto ORDER BY id_producto")
-    productos= cur.fetchall()
+    consulta= conexion.cursor()
+    consulta.execute("SELECT id_producto, nombre FROM producto ORDER BY id_producto")
+    productos= consulta.fetchall()
     if productos:
-        print("\n---Inventario---")
+        print("\n---INVENTARIO---")
         for producto in productos:
             print(f"ID: {producto[0]}, Nombre: {producto[1]}")
     else:
-        print("\n---No hay productos en el inventario---")
+        print("\n---INVENTARIO VACÍO---")
 
 
 #El administrador puede seleccionar un producto y actualizar su
 #cantidad en stock o precio.
 def actualizar_inventario(conexion):
     mostrar_inventario(conexion)
-    producto_id= int(input("\nIngrese el ID del producto: "))
-    nuevo_precio= int(input("Nuevo precio del producto: "))
+    producto_id= int(input("\nSeleccionar el ID del producto: "))
+    nuevo_precio= int(input("Nuevo precio: "))
     nueva_cant_stock= int(input("Nueva cantidad en stock: "))
-    cur= conexion.cursor()
-    cur.execute("UPDATE producto SET precio = %s, cant_stock = %s WHERE id_producto = %s", (nuevo_precio, nueva_cant_stock, producto_id))
+    consulta= conexion.cursor()
+    consulta.execute("UPDATE producto SET precio = %s, cant_stock = %s WHERE id_producto = %s", (nuevo_precio, nueva_cant_stock, producto_id))
     conexion.commit()
-    print("\n---Inventario actualizado con éxito---")
+    print("\n---INVENTARIO ACTUALIZADO---")
 
 #El sistema muestra una lista de los productos 
 #que tienen una cantidad en stock por debajo de un umbral definido*, junto con su información
 def ver_productos_bajo_stock(conexion, cantidad_minima=10):
-    cur= conexion.cursor()
-    cur.execute("SELECT id_producto, nombre, descripcion, precio, cant_stock FROM producto WHERE cant_stock < %s ORDER BY id_producto", (cantidad_minima,))
-    productos= cur.fetchall()#Captura la tupla de producto
-    print("\n---Productos Bajos en Stock---")
+    consulta= conexion.cursor()
+    consulta.execute("SELECT id_producto, nombre, descripcion, precio, cant_stock FROM producto WHERE cant_stock < %s ORDER BY id_producto", (cantidad_minima,))
+    productos= consulta.fetchall()#Captura la tupla de producto
+    print("\n---PRODUCTOS BAJO STOCK (10)---")
     for producto in productos:
         print(f"ID: {producto[0]}, Nombre: {producto[1]}, Descripción: {producto[2]}, Precio: {producto[3]}, Stock: {producto[4]}")
 
@@ -169,31 +169,31 @@ def ver_productos_bajo_stock(conexion, cantidad_minima=10):
 def registrar_venta(conexion):
     email_user= input("Email del usuario: ")
     listar_productos(conexion)
-    producto_id= int(input("\nIngrese el ID del producto que desea vender: "))
+    producto_id= int(input("\nSeleccionar ID del producto: "))
     cantidad= int(input("Ingrese la cantidad: "))
-    cur= conexion.cursor()
-    cur.execute("SELECT precio, cant_stock FROM producto WHERE id_producto = %s", (producto_id,))
-    producto= cur.fetchone()
+    consulta= conexion.cursor()
+    consulta.execute("SELECT precio, cant_stock FROM producto WHERE id_producto = %s", (producto_id,))
+    producto= consulta.fetchone()
     
     if ((producto) and (producto[1] >= cantidad)):#Si el producto != None y el stock es mayor o igual a la cantidad solicitada
         precio_total= producto[0] * cantidad
         nuevo_stock= producto[1] - cantidad
 
-        cur.execute("INSERT INTO venta (email, id_producto, monto_total) VALUES (%s, %s, %s)", (email_user, producto_id, precio_total))
-        cur.execute("UPDATE producto SET cant_stock = %s WHERE id_producto = %s", (nuevo_stock, producto_id))
+        consulta.execute("INSERT INTO venta (email, id_producto, monto_total) VALUES (%s, %s, %s)", (email_user, producto_id, precio_total))
+        consulta.execute("UPDATE producto SET cant_stock = %s WHERE id_producto = %s", (nuevo_stock, producto_id))
         conexion.commit()
-        print("\n---Venta registrada con éxito---")
+        print("\n---VENTA REGISTRADA---")
     else:
         conexion.rollback()
-        print("\n---Stock insuficiente o producto no encontrado---")
+        print("\n---STOCK INSUFICIENTE O NO EXISTE EL PRODUCTO---")
             
 #El sistema muestra un registro de las ventas realizadas, 
 #incluyendo los productos vendidos y el monto total
 def ver_historial_ventas(conexion):
-    cur= conexion.cursor()
-    cur.execute("SELECT id_venta, email, id_producto, monto_total FROM venta")
-    ventas= cur.fetchall()#Se captura la tupla
-    print("\n---Historial de Ventas---")
+    consulta= conexion.cursor()
+    consulta.execute("SELECT id_venta, email, id_producto, monto_total FROM venta")
+    ventas= consulta.fetchall()#Se captura la tupla
+    print("\n---HISTORIAL DE VENTAS---")
     for venta in ventas:
         print(f"ID Venta: {venta[0]}, Email: {venta[1]}, ID Producto: {venta[2]}, Monto Total: {venta[3]}")
 
@@ -206,19 +206,19 @@ def menu_usuario(conexion, email):
         print("2) Ver catálogo de productos")
         print("3) Realizar una compra")
         print("4) Salir")
-        opcion= input("\nElige una opción: ")
+        opcion= int(input("\nElige una opción: "))
 
-        if (opcion == '1'):
+        if (opcion == 1):
             ver_informacion_personal(conexion, email)
-        elif (opcion == '2'):
+        elif (opcion == 2):
             listar_productos(conexion)
-        elif (opcion == '3'):
+        elif (opcion == 3):
             realizar_compra(conexion, email)
-        elif (opcion == '4'):
-            print("\n---Saliendo del sistema---")
+        elif (opcion == 4):
+            print("\n---SALIENDO...---")
             break
         else:
-            print("Opción no válida.")
+            print("OPCIÓN NO VÁLIDA")
 
 def menu_admin(conexion, email):
     while True:
@@ -230,25 +230,25 @@ def menu_admin(conexion, email):
         print("5) Registrar una venta")
         print("6) Ver historial de ventas")
         print("7) Salir")
-        opcion= input("\nElige una opción: ")
+        opcion= int(input("\nElige una opción: "))
 
-        if (opcion == '1'):
+        if (opcion == 1):
             registrar_producto(conexion)
-        elif (opcion == '2'):
+        elif (opcion == 2):
             ver_detalles_producto(conexion)
-        elif (opcion == '3'):
+        elif (opcion == 3):
             actualizar_inventario(conexion)
-        elif (opcion == '4'):
+        elif (opcion == 4):
             ver_productos_bajo_stock(conexion)
-        elif (opcion == '5'):
+        elif (opcion == 5):
             registrar_venta(conexion)
-        elif (opcion == '6'):
+        elif (opcion == 6):
             ver_historial_ventas(conexion)
-        elif (opcion == '7'):
-            print("\n---Saliendo del sistema---")
+        elif (opcion == 7):
+            print("\n---SALIENDO...---")
             break
         else:
-            print("Opción no válida.")
+            print("OPCIÓN NO VÁLIDA")
 
 '''---------------INICIO DEL PROGRAMA--------------'''
 
@@ -258,9 +258,9 @@ while True:
     print("1) Registrarse")
     print("2) Iniciar sesión")
     print("3) Salir")
-    opcion= input("\nElige una opción: ")
+    opcion= int(input("\nElige una opción: "))
 
-    if (opcion == '1'):
+    if (opcion == 1):
         print("------------------")
         print("     REGISTRO     ")
         print("------------------")
@@ -269,7 +269,7 @@ while True:
         password= input("Contraseña: ")
         registrar_usuario(conexion, nombre, email, password, 'usuario') # todos los usuarios registrados serán usuarios normales
     
-    elif (opcion == '2'):
+    elif (opcion == 2):
         print("------------------")
         print("  INICIAR SESION  ")
         print("------------------")
@@ -282,13 +282,13 @@ while True:
         elif (tipo_usuario == 'admin'):
             menu_admin(conexion, email)
             
-    elif (opcion == '3'):
+    elif (opcion == 3):
         print("------------------")
         print("     SALIENDO     ")
         print("------------------")
         break
     else:
-        print("Opción no válida.")
+        print("OPCIÓN NO VÁLIDA")
 
 conexion.close()
 
